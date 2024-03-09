87bf1a02a36c6c1699f848d4f349f33c9b9d36b7
iharuya <i4haruya@gmail.com>
2024-03-10 01:56:32 +0900

    Initial Commit

---
+++ SpeedMeter/SpeedMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedMeterApp.swift
+//  SpeedMeter
+//
+//  Created by Haruya Ishikawa on 2024/03/10.
+//
+
+import SwiftUI
+
+@main
+struct SpeedMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
